83e39fb66fd8648e17d24bbb54325cb01d47f83f
Ulici Ioana-Anamaria <ulici.ioana@yahoo.com>
2024-02-15 17:01:51 +0900

    First iteration of adding force/torque reference, works with fixed impedance mode for now

--- robosuite/controllers/__init__.py
+++ robosuite/controllers/__init__.py
@@ -1,5 +1,6 @@
 from .controller_factory import controller_factory, load_controller_config, reset_controllers, get_pybullet_server
 from .osc import OperationalSpaceController
+from .oscFT import OperationalSpaceControllerFT
 from .joint_pos import JointPositionController
 from .joint_vel import JointVelocityController
 from .joint_tor import JointTorqueController
@@ -11,6 +12,9 @@ CONTROLLER_INFO = {
     "JOINT_POSITION": "Joint Position",
     "OSC_POSITION": "Operational Space Control (Position Only)",
     "OSC_POSE": "Operational Space Control (Position + Orientation)",
+    "OSC_POSITION_FT": "Operational Space Control with force reference (Position Only)",
+    "OSC_POSE_FT": "Operational Space Control with force reference (Position + Orientation)",
+
     "IK_POSE": "Inverse Kinematics Control (Position + Orientation) (Note: must have PyBullet installed)",
 }
 
