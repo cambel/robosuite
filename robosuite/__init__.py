13f2d0761f09454c87090717da379c0278e4ae68
Cristian C Beltran-Hernandez <cristianbehe@gmail.com>
2024-07-05 05:12:56 +0000

    Add compliance controller

--- robosuite/__init__.py
+++ robosuite/__init__.py
@@ -16,6 +16,7 @@ from robosuite.environments.manipulation.two_arm_osx import TwoArmOSX
 from robosuite.environments.manipulation.two_arm_wiping import TwoArmWiping
 from robosuite.environments.manipulation.two_arm_wiping_diff import TwoArmWipingDiff
 from robosuite.environments.manipulation.grind import Grind
+from robosuite.environments.manipulation.osx_grind import OSXGrind
 
 from robosuite.environments import ALL_ENVIRONMENTS
 from robosuite.controllers import ALL_CONTROLLERS, load_controller_config
